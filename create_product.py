9d631807eca27030599f71b08a63e7f9255e1d0b
akhileshku5372-crypto <akhileshku5372@gmail.com>
2026-09-10 23:24:10 +0530

    Update create_product.py

--- create_product.py
+++ create_product.py
@@ -15,7 +15,7 @@ def generate_content():
     Keep it clean, concise, and professional with bullet points. Do not include markdown stars (**) in headings so it renders cleanly.
     """
     completion = client.chat.completions.create(
-        model="llama3-8b-8192",
+        model="llama-3.1-8b-instant",,
         messages=[{"role": "user", "content": prompt}],
         temperature=0.6,
         max_tokens=1500
